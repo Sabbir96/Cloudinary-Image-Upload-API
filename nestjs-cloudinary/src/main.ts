8758e4cea07e8a9fb2e14ee9db78b5b2307f29a3
Sabbir96 <sabbir21islam@gmail.com>
2023-12-05 16:49:42 +0600

    cors setup

--- nestjs-cloudinary/src/main.ts
+++ nestjs-cloudinary/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  const corsOptions: CorsOptions = {
+    origin: '*', // Replace '*' with the desired origin or origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, authorization',
+  };
+  app.enableCors(corsOptions);
 }
 bootstrap();
